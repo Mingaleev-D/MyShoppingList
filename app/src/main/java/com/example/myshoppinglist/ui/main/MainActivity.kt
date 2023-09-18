1feea85ea42f17e2ccdfd3fae6d8dd05dc75b876
admin <mingaleev.d92@mail.ru>
2023-09-18 23:45:06 +0300

    Initial commit

---
+++ app/src/main/java/com/example/myshoppinglist/ui/main/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.example.myshoppinglist.ui.main
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.myshoppinglist.databinding.ActivityMainBinding
+
+
+class MainActivity : AppCompatActivity() {
+   val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
+
+   override fun onCreate(savedInstanceState: Bundle?) {
+      super.onCreate(savedInstanceState)
+      setContentView(binding.root)
+   }
+}
